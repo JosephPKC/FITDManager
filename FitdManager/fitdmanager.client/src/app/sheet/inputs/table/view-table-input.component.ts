cd7375f303ccce0c5091e8882e03c2c51e0fa905
Joseph Park <josephpkc@gmail.com>
2025-03-03 17:42:12 -0800

    (feat): Add the view table file

--- FitdManager/fitdmanager.client/src/app/sheet/inputs/table/view-table-input.component.ts
+++ FitdManager/fitdmanager.client/src/app/sheet/inputs/table/view-table-input.component.ts
@@ -1,9 +1,8 @@
 import {
-  Component, InputSignal, InputSignalWithTransform, OnChanges, OnInit, Signal, WritableSignal,
-  booleanAttribute,
-  computed, forwardRef, input, numberAttribute, signal
+  Component, InputSignal, Signal, SimpleChanges, WritableSignal,
+  computed, forwardRef, input, signal
 } from "@angular/core";
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
+import { NG_VALUE_ACCESSOR } from "@angular/forms";
 
 import { BaseTableInputDirective } from "./base-table-input.directive";
 import { BaseInputDirective } from "../base-input.directive";
@@ -12,29 +11,45 @@ import { BaseInputDirective } from "../base-input.directive";
  * Table input that only displays its contents.
  * The user has no direct ability to change the contents within this input.
  */
-//@Component({
-//  selector: "view-table-input",
-//  templateUrl: "view-table-input.component.html",
-//  styleUrl: "view-table-input.component.scss",
-//  providers: [
-//    {
-//      provide: NG_VALUE_ACCESSOR,
-//      useExisting: forwardRef(() => ViewTableInputComponent),
-//      multi: true
-//    }
-//  ],
-//  standalone: true
-//})
-//export class ViewTableInputComponent extends BaseInputDirective<string[]> {
-//  // #region Inputs
+@Component({
+  selector: "view-table-input",
+  templateUrl: "view-table-input.component.html",
+  styleUrl: "view-table-input.component.scss",
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ViewTableInputComponent),
+      multi: true
+    }
+  ],
+  standalone: true
+})
+export class ViewTableInputComponent extends BaseInputDirective<string[]> {
+  // #region Inputs
+  public header: InputSignal<string> = input<string>("");
+  public footer: InputSignal<string> = input<string>("");
+  public enforceUnique: InputSignal<boolean> = input<boolean>(false);
+  // #endregion
 
-//  // #endregion
+  // #region State
+  protected items: WritableSignal<string[]> = signal<string[]>([]);
+  // #endregion
 
-//  // #region State
+  // #region Computes
+  protected shouldCreateHeader: Signal<boolean> = computed<boolean>(() => {
+    return this.header() !== "";
+  });
 
-//  // #endregion
+  protected shouldCreateFooter: Signal<boolean> = computed<boolean>(() => {
+    return this.footer() !== "";
+  });
 
-//  // #region Computes
-//  // #endregion
 
-//}
+  // #endregion
+
+  // #region BaseInputDirective
+  public override writeValue(val: string[]): void {
+    this.items.set(val.slice());
+  }
+  // #endregion
+}
